730a0d84da7e18c625169fcf8cb3af3756564fd1
mkao <mondulkao@hotmail.com>
2022-02-07 12:37:28 -0500

    forgot to add the app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import './App.css';
 import React, { useState, useCallback, useRef, useEffect } from "react";
 import UseEffectExample from './components/UseEffectExample';
 import TimerExample from './components/TimerExample';
-
-window.TIME = 0;
-
-setInterval(function () { window.TIME += 1 }, 1000);
+import GridWorld from './components/GridWorld';
 
 function App() {
   return (
     <>
-      {/*UseEffectExample></UseEffectExample>*/}
-      <TimerExample></TimerExample>
+      {/*<UseEffectExample></UseEffectExample>*/}
+      {/*<TimerExample></TimerExample>*/}
+      <GridWorld></GridWorld>
     </>
   )
 }
